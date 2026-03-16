613d93b3b4e0d78f2b66d3775bd953e0efa77502
Baskuik <97105236@st.deltion.nl>
2026-03-16 11:53:02 +0100

    sad

--- resources/views/Create.blade.php
+++ resources/views/Create.blade.php
@@ -13,7 +13,7 @@
                 <label class="block mb-1">Label Naam:</label>
                 <input type="text" name="label_name" class="w-full border p-2 rounded" required>
             </div>
-                <button type="submit" class="bg-green-600 text-white px-6 py-2 rounded hover:bg-green-700">
+                <button type="s ubmit" class="bg-green-600 text-white px-6 py-2 rounded hover:bg-green-700">
                     Opslaan
                 </button>
             </div>
